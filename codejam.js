d5c5958496121d3cbec9e9b4537024eaca8fcb76
kipernicus <kip.armstrong@gmail.com>
2018-11-09 13:48:50 -0700

    Add scaffolding for gathering input and creating output

--- codejam.js
+++ codejam.js
@@ -1,5 +1,29 @@
+/*
+This is an attempt at solving the Google Code Jam Problem C located at
+https://code.google.com/codejam/contest/90101/dashboard#s=p2
+ */
+
+const SAMPLE_INPUT = [
+  '3',
+  'elcomew elcome to code jam',
+  'wweellccoommee to code qps jam',
+  'welcome to codejam'
+]
+
+function getInput() {
+  // TODO: Add I/O support to get the real input
+  return SAMPLE_INPUT
+}
+
+function createOutput(output) {
+  // TODO: Add I/O support to output in the correct manner
+  console.log(output)
+}
+
 function run() {
-  console.log('Running...')
+  const input = getInput()
+  const output = executeSolution(input)
+  return createOutput(output)
 }
 
 run()
